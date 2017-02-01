f33306ecb8afae9ba45ca9254c9b7911dc47fe67
TharshanSIVAPALAN <sivapalan.tharshan@gmail.com>
2017-02-01 13:01:58 +0100

    all ok but the fatal error

--- tests/Lib/StringFormatterTest.php
+++ tests/Lib/StringFormatterTest.php
@@ -17,17 +17,40 @@ class StringFormatterTest extends \PHPUnit_Framework_TestCase
     public function testConcatStringError()
     {
      
-    
-
-
-
     }
 
     public function testToCamelCase()
     {
         $myString = new StringFormatter();
         $result = $myString->toCamelCase("au", "revoir");
+        $this->assertEquals("auRevoir", $result);
+    }
 
-        $this->assertEquals("aurevoir", $result);
+ 	public function testPrefixIsFalseParameterSuccess()
+    {
+        $myString = new StringFormatter();
+        $result = $myString->prefix("see", "you", false);
+        $this->assertEquals("seeyou", $result);
+    }
+
+    public function testPrefixIsTrueParameterSuccess()
+    {
+        $myString = new StringFormatter();
+        $result = $myString->prefix("good", "food", true);
+        $this->assertEquals("goodFood", $result);
+    }
+
+    public function testSuffixIsFalseParameterSuccess()
+    {
+        $myString = new StringFormatter();
+        $result = $myString->suffix("au", "revoir", false);
+        $this->assertEquals("revoirau", $result);
+    }
+
+    public function testSuffixIsTrueParameterSuccess()
+    {
+        $myString = new StringFormatter();
+        $result = $myString->suffix("au", "revoir", true);
+        $this->assertEquals("revoirAu", $result);
     }
 }
